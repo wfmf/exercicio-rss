6871a3dc9bd20ecad0ec5f661af779ff4d40fa54
Wellington Felix <wfmf@cin.ufpe.br>
2018-09-04 22:58:39 -0300

    Adiciona código dos passos 1 ao 3

--- RSS/app/src/main/java/br/ufpe/cin/if710/rss/MainActivity.kt
+++ RSS/app/src/main/java/br/ufpe/cin/if710/rss/MainActivity.kt
@@ -2,11 +2,39 @@ package br.ufpe.cin.if710.rss
 
 import android.app.Activity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import org.jetbrains.anko.doAsync
+import org.jetbrains.anko.uiThread
+import java.io.IOException
+import java.net.URL
 
 class MainActivity : Activity() {
+    private val feedUrl = "http://pox.globo.com/rss/g1/brasil/"
+
+    //OUTROS LINKS PARA TESTAR...
+    //http://rss.cnn.com/rss/edition.rss
+    //http://pox.globo.com/rss/g1/brasil/
+    //http://pox.globo.com/rss/g1/ciencia-e-saude/
+    //http://pox.globo.com/rss/g1/tecnologia/
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        try {
+            doAsync {
+                val feedXML = getRssFeed(feedUrl)
+                uiThread { conteudoRSS.text = feedXML }
+            }
+        } catch(e: IOException) {
+            e.printStackTrace()
+        }
+    }
+
+    private fun getRssFeed(feed: String): String {
+        return URL(feed).readText()
     }
 }
