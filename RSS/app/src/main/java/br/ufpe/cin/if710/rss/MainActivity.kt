d9895e151e99c96c4b54f25e99f0b86e18fc6342
Wellington Felix <wfmf@cin.ufpe.br>
2018-09-29 11:19:29 -0300

    Adiciona código do passo 9

--- RSS/app/src/main/java/br/ufpe/cin/if710/rss/MainActivity.kt
+++ RSS/app/src/main/java/br/ufpe/cin/if710/rss/MainActivity.kt
@@ -1,9 +1,13 @@
 package br.ufpe.cin.if710.rss
 
 import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import android.preference.PreferenceManager
 import android.support.v7.widget.DividerItemDecoration
 import android.support.v7.widget.LinearLayoutManager
+import android.view.Menu
+import android.view.MenuItem
 import android.view.View.GONE
 import kotlinx.android.synthetic.main.activity_main.*
 import org.jetbrains.anko.doAsync
@@ -11,44 +15,76 @@ import org.jetbrains.anko.uiThread
 import java.io.IOException
 import java.net.URL
 
+//OUTROS LINKS PARA TESTAR...
+//http://rss.cnn.com/rss/edition.rss
+//http://pox.globo.com/rss/g1/brasil/
+//http://pox.globo.com/rss/g1/ciencia-e-saude/
+//http://pox.globo.com/rss/g1/tecnologia/
+
 class MainActivity : Activity() {
     private var feedUrl = ""
     private var adapter = ItemRSSAdapter(listOf(), this)
 
-    //OUTROS LINKS PARA TESTAR...
-    //http://rss.cnn.com/rss/edition.rss // Não funciou, erro de parsing
-    //http://pox.globo.com/rss/g1/brasil/
-    //http://pox.globo.com/rss/g1/ciencia-e-saude/
-    //http://pox.globo.com/rss/g1/tecnologia/
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_main)
-        feedUrl = getString(R.string.rssfeed)
         conteudoRSS.layoutManager = LinearLayoutManager(this)
         conteudoRSS.addItemDecoration(DividerItemDecoration(this, LinearLayoutManager.VERTICAL))
+
         // Para evitar o erro "No adapter attached; skipping layout" criei primeiro um adapter com uma
         // lista vazia e depois que a requisição é resolvida mudo a lista associada ao adapter
         conteudoRSS.adapter = adapter
+        val sharedPref = PreferenceManager.getDefaultSharedPreferences(this)
+        feedUrl = sharedPref.getString(getString(R.string.rss_url_key), "http://leopoldomt.com/if1001/g1brasil.xml")
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.settings_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        when(item?.itemId) {
+            R.id.settings_button -> startActivity(Intent(this@MainActivity, PrefsMenuActivity::class.java))
+        }
+        return super.onOptionsItemSelected(item)
     }
 
     override fun onStart() {
         super.onStart()
         try {
-            // Inicia tarefa assíncrona
-            doAsync {
-                val rawXML = getRssFeed(feedUrl)
-                val parsedContent: List<ItemRSS> = ParserRSS.parse(rawXML)
-                // Esconde o progess indicator e muda a lista
-                // de itens do RSS
-                uiThread {
-                    adapter.items = parsedContent
-                    // GONE faz uma view não ser visível e não tomar espaço
-                    progressIndicator.visibility = GONE
-                }
-            }
+            fetchRSS()
         } catch(e: IOException) {
             e.printStackTrace()
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        try {
+            feedUrl = PreferenceManager
+                    .getDefaultSharedPreferences(this)
+                    .getString(getString(R.string.rss_url_key), "")
+            fetchRSS()
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+    }
+
+    private fun fetchRSS() {
+        // Inicia tarefa assíncrona
+        doAsync {
+            val rawXML = getRssFeed(feedUrl)
+            val parsedContent: List<ItemRSS> = ParserRSS.parse(rawXML)
+            // Esconde o progess indicator e muda a lista
+            // de itens do RSS
+            uiThread {
+                adapter.items = parsedContent
+                conteudoRSS.adapter = adapter
+                // GONE faz uma view não ser visível e não tomar espaço
+                progressIndicator.visibility = GONE
+            }
         }
     }
 
---
+++ RSS/app/src/main/java/br/ufpe/cin/if710/rss/PrefsMenuActivity.kt
@@ -0,0 +1,44 @@
+package br.ufpe.cin.if710.rss
+
+import android.os.Bundle
+import android.app.Activity
+import android.content.SharedPreferences
+import android.preference.Preference
+import android.preference.PreferenceFragment
+import android.preference.PreferenceManager
+
+class PrefsMenuActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.preferencias)
+    }
+
+    class RSSFeedPreferenceFragment : PreferenceFragment() {
+        private var mListener: SharedPreferences.OnSharedPreferenceChangeListener? = null
+        private var mRSSFeedURL: Preference? = null
+
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+            addPreferencesFromResource(R.xml.feed_prefs)
+
+            val prefs  = PreferenceManager.getDefaultSharedPreferences(activity)
+            mRSSFeedURL = preferenceManager.findPreference(getString(R.string.rss_url_key))
+
+            mRSSFeedURL?.summary = PreferenceManager.getDefaultSharedPreferences(activity)
+                    .getString(getString(R.string.rss_url_key), "Error")
+
+
+            mListener = SharedPreferences.OnSharedPreferenceChangeListener { sharedPreferences, key ->
+                val newValue = sharedPreferences.getString(getString(R.string.rss_url_key), "Unknown url")
+                mRSSFeedURL?.summary = newValue
+            }
+
+            prefs.registerOnSharedPreferenceChangeListener(mListener)
+
+            mListener?.onSharedPreferenceChanged(prefs, getString(R.string.rss_url_key ))
+        }
+
+    }
+
+}
